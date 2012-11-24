6a2eab742fd29dadeeadd4170c3bcbe13863f3ab
Kevin Barabash <kevinb7@gmail.com>
2012-11-24 13:16:01 -0700

    adding main.ts and updated contact manager

--- contacts/typescript/contactManager.ts
+++ contacts/typescript/contactManager.ts
@@ -2,8 +2,22 @@ declare var $: any;
 
 class ContactManager {
     constructor() {
-        $('li').each(function (index, elem) {
-            console.log(elem);
+        var cm = this;
+        $('#new-contact').click(function () {
+            cm.newContact();
         });
+    };
+
+    newContact() {
+        var div$ = $('<div></div>')
+                .css({
+                    position:'absolute',
+                    left:100,
+                    top:100,
+                    width:500,
+                    height:300,
+                    border: 'solid 1px black'
+                });
+        div$.appendTo(document.body);
     };
 }
---
+++ contacts/typescript/main.ts
@@ -0,0 +1,6 @@
+declare var ContactManager: any;
+declare var $: any;
+
+$(document).ready(function () {
+    var cm = new ContactManager();
+});
