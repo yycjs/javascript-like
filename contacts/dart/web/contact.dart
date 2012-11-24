7ed079e949de7859d26565b217f4d8a37b2ffd59
David Luecke <daff@neyeon.com>
2012-11-24 13:10:31 -0700

    Contacts

--- contacts/dart/web/contact.dart
+++ contacts/dart/web/contact.dart
@@ -1,19 +1,49 @@
 library contacts;
 
+import 'dart:html';
+
 class Address {
   
 }
+
 class Contact {
+  num id;
   String name;
   String phone;
   String email;
-  Address homeAddress;
+  Address address;
   String image;
   
   Contact(this.name, this.email);
   
+  Element render() {
+    return new Element.html(this.toString());
+  }
+  
   String toString() {
-    // '<div>{{name}}</div>'
-    return "";
+    return '''<li class="contact span8">
+        <a href="javascript://" class="remove"><i class="icon-remove"></i></a>
+        <div class="row">
+          <div class="span2">
+            <img src="img/contact.png" width="100" height="100">
+           </div>
+          <div class="span3">
+            <input type="text" name="name" placeholder="Add Name" value="${this.name}">
+            <select name="category">
+              <option value="family">Family</option>
+              <option value="friends">Friends</option>
+              <option value="co-workers" selected="">Co-workers</option>
+            </select>
+          </div>
+        <div class="span3">
+          <label>Address</label>
+          <input type="text" name="address" value="${this.address.toString()}">
+          <label>Phone</label>
+          <input type="text" name="phone" value="${this.phone}">
+          <label>Email</label>
+          <input type="text" name="email" value="${this.email}">
+        </div>
+      </div>
+    </li>''';
   }
 }
--- contacts/dart/web/dart.dart
+++ contacts/dart/web/dart.dart
@@ -3,14 +3,5 @@ import 'contact.dart';
 
 void main() {
   Contact c = new Contact("Test", "test@example.com");
-  window.console.log();
-}
-
-void reverseText(Event event) {
-  var text = query("#text").text;
-  var buffer = new StringBuffer();
-  for (int i = text.length - 1; i >= 0; i--) {
-    buffer.add(text[i]);
-  }
-  query("#text").text = buffer.toString();
+  query('#contacts ul').elements.add(c.render());
 }
