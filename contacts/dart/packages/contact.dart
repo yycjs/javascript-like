f8c606d8b292f50c31c1fade9b4a2c725715cd14
Joaquin Windmuller <joaquin@windmuller.ca>
2012-11-24 12:22:07 -0700

    Initial Contact and Address shells

---
+++ contacts/dart/packages/contact.dart
@@ -0,0 +1,19 @@
+class Address {
+  
+}
+class Contacts {
+  String name;
+  String phone;
+  String email;
+  Address homeAddress;
+  String image;
+  
+  Contacts(this.name, this.email);
+  
+  String toString() {
+    return "";
+  }
+  
+  
+  
+}
